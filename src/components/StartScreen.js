9a22092d2f7324a5cef5e8ac84fcce98ad837291
thomas <thomascaraujo19@gmail.com>
2023-12-25 15:55:55 -0300

    aprov

---
+++ src/components/StartScreen.js
@@ -0,0 +1,15 @@
+import "./StartScreen.css";
+
+const StartScren = () => {
+  return (
+    <div className = "start">
+        
+        <h1>Secret Word</h1>
+
+        <p>Clique no botão para iniciar o jogo!</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScren
