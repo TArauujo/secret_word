21506e6383826ec617e35f138f5a6be69eb78ef5
thomas <thomascaraujo19@gmail.com>
2023-12-29 02:14:29 -0300

    aprov

--- src/App.js
+++ src/App.js
@@ -29,14 +29,19 @@ function App() {
 
   //process the letter input
   const verifyLetter = () => {
-    setGameStage(stages[2].name);
-  }
+    setGameStage(stages[2].name)
+  };
+
+  //restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  };
 
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame = {startGame}/>}
       {gameStage === "game" && <Game verifyLetter = {verifyLetter}/>}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "end" && <GameOver retry = {retry}/>}
 
     </div>
   );
