b9c68fe6848a12120b0f0b3095d802f205de1461
thomas <thomascaraujo19@gmail.com>
2024-01-02 02:32:06 -0300

    aprov

--- src/App.js
+++ src/App.js
@@ -22,8 +22,29 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordList);
 
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] =  useState([]);
+
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+  };
+
   //start the secret word game
   const startGame = () =>{
+    // pick word and pick category
+    pickWordAndCategory();
+    
     setGameStage(stages[1].name)
   };
 
