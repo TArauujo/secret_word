cd03dba0fa3314a9f9ae5379217736f2eb20acf4
thomas <thomascaraujo19@gmail.com>
2024-01-04 21:12:14 -0300

    aprov

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -13,6 +13,18 @@ const Game = ({
 }) => {
 
   const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus();
+  };
+
   return (
 
     <div className="game"> 
@@ -37,8 +49,14 @@ const Game = ({
       </div>
       <div className="letterContainer"> {/* Tentativa de acertar as letras da palavra */}
         <p>Tente adivinhar uma letra da palavra: </p>
-        <form>
-          <input type="text" name="letter" maxLength= "1" required onChange={(e) => setLetter(e.target.value)}/>
+        <form onSubmit = {handleSubmit}>
+          <input type="text" 
+            name="letter" 
+            maxLength= "1" 
+            required onChange={(e) => setLetter(e.target.value)}
+            value = {letter} 
+            ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
